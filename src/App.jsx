abd6ee25db77269f0986861bfda91523e259c040
chakshu <chakshushandilya@gmail.com>
2024-03-08 11:07:54 +0530

    added react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/starred" element={<Starred />} />
+          </Route>
 
-        <Route path="/show/:showId" element={<Show />} />
+          <Route path="/show/:showId" element={<Show />} />
 
-        <Route path="*" element={<div> Bhg ja </div>} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="*" element={<div> Bhg ja </div>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
