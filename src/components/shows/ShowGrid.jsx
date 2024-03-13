6778f9a3596262970ca74fb04c38cd90448be6cb
chakshu <chakshushandilya@gmail.com>
2024-03-13 16:54:34 +0530

    made starred show working

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -1,38 +1,8 @@
-import { useReducer, useEffect } from 'react';
+import { useStarredShows } from '../../lib/useStarredShows';
 import ShowCard from './ShowCard';
 
-const usePersistedReducer = (reducer, initialState, localStorageKey) => {
-  const [state, dispatch] = useReducer(reducer, initialState, initial => {
-    const persistedValue = localStorage.getItem(localStorageKey);
-
-    return persistedValue ? JSON.parse(persistedValue) : initial;
-  });
-
-  useEffect(() => {
-    localStorage.setItem(localStorageKey, JSON.stringify(state));
-  }, [state, localStorageKey]);
-
-  return [state, dispatch];
-};
-
-const starredShowReducer = (currentStarred, action) => {
-  switch (action.type) {
-    case 'STAR':
-      return currentStarred.concat(action.showId);
-    case 'UNSTAR':
-      return currentStarred.filter(showId => showId !== action.showId);
-    default:
-      return currentStarred;
-  }
-};
-
 const ShowGrid = ({ shows }) => {
-  const [starredShows, dispatchStarred] = usePersistedReducer(
-    starredShowReducer,
-    [],
-    'starredShows'
-  );
-
+  const [starredShows, dispatchStarred] = useStarredShows();
   const onStarMeClick = showId => {
     const isStarred = starredShows.includes(showId);
 
@@ -55,6 +25,7 @@ const ShowGrid = ({ shows }) => {
           }
           summary={data.show.summary}
           onStarMeClick={onStarMeClick}
+          isStarred={starredShows.includes(data.show.id)}
         />
       ))}
     </div>
